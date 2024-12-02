58fd6066a47030c2035f0703dd00a30a3de70346
jeev86399 <nnani86399@gmail.com>
2024-12-02 16:05:31 +0530

    Add files via upload

---
+++ stringformat.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class stringformat{
+    public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    String a=sc.next();
+    String b=String.format("congraluations %s you won 5 lacks",a);
+    System.out.println(b);
+}
+}
