6d06844111663823053a1cf74c60cc6a53371569
jeev86399 <nnani86399@gmail.com>
2024-12-02 15:16:47 +0530

    Add files via upload

---
+++ printnext.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class printnext
+{
+    public static void main(String args[])
+    {
+        char a='A';
+        int b=a;
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+    }
+}
