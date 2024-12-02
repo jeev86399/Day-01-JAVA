7f0c51dcee111f605405460f0372ac679b4e5c00
jeev86399 <nnani86399@gmail.com>
2024-12-02 14:50:12 +0530

    Add files via upload

---
+++ dt.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class dt{
+    public static void main(String arge[])
+    {
+        int a=12;
+        char b='M';
+        float c=4.567f;
+        boolean d=false;
+        double e=24.5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
