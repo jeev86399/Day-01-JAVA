5ecc3b428c3fbd5da0d1346062917fe39ba575be
jeev86399 <nnani86399@gmail.com>
2024-12-02 16:19:04 +0530

    Add files via upload

---
+++ nameage.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class nameage{
+    public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    String a=sc.next();
+    int b=sc.nextInt();
+    System.out.printf("the name of the person is %s and age is %d",a,b);
+}
+}
